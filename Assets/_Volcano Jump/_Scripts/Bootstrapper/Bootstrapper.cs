c72ba9b229a869d28e40c29839c4413a1f0c1b26
NoSlovo <88000429+NoSlovo@users.noreply.github.com>
2024-06-19 10:57:47 +0500

    AddRestart

--- Assets/_Volcano Jump/_Scripts/Bootstrapper/Bootstrapper.cs
+++ Assets/_Volcano Jump/_Scripts/Bootstrapper/Bootstrapper.cs
@@ -9,18 +9,18 @@ namespace _Volcano_Jump._Scripts.Bootstrapper
     {
         [SerializeField] private GameLifeTineScope _gameLifeTineScope;
         [SerializeField] private RectTransform _rotationLogo;
-        
+
         private GameFSM _gameFsm;
 
         private void Start()
         {
             _rotationLogo.DORotate(new Vector3(0, 0, -360f), 1f, RotateMode.FastBeyond360)
                 .SetLoops(-1, LoopType.Restart)
-                .SetEase(Ease.Linear); 
+                .SetEase(Ease.Linear);
             _gameFsm = new GameFSM(_gameLifeTineScope.Container);
             _gameFsm.Init();
             _gameFsm.Enter<LoaderState>();
-            
+
             DontDestroyOnLoad(this);
         }
     }
